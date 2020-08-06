176772ecccd9d69cbab738af5733454071ecc07b
January <januaryobrien.ux@gmail.com>
2020-08-06 11:55:51 -0400

    completed adding to list

--- src/index.js
+++ src/index.js
@@ -17,19 +17,22 @@ let todos = [
         description: "Call the caterers"
     }
 ];
+let app = document.querySelector("#app");
+let h1 = document.createElement("h1");
+let todoList= document.createElement("ul");
+// your code here
+
+h1.innerText = "Todo List";
+app.appendChild(h1);
+app.appendChild(todoList);
 function renderTodoApp() {
-    let app = document.querySelector("#app");
-    let h1 = document.createElement("h1");
-    let todoList= document.createElement("ul");
-    // your code here
-    h1.innerText = "Todo List";
-    app.appendChild(h1);
-    app.appendChild(todoList);
+    todoList.innerHTML = ""
+    let todoMarkUp = "";
 
     for (let i=0; i < todos.length; i++) {
         let todoItems = todos[i]
         let stateOfBox = todoItems.completed ? "checked": "";
-        let todoMarkUp = `
+        todoMarkUp = `
         <li><input type= "checkbox" ${stateOfBox} >
     
         ${todoItems.description}
@@ -37,10 +40,16 @@ function renderTodoApp() {
         `;
         todoList.innerHTML += todoMarkUp;
     }
+    
+    
 
     // and maybe some here
- 
     // app.append(todoListElement)
 }
-
 renderTodoApp();
+function onButtonClick() {
+    let inputElement = document.querySelector("#input").value;
+    let object = {completed:false, description:inputElement};
+    todos.push(object);
+    renderTodoApp();
+}
